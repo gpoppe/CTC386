c00625d8f32022ebc9896a70fc31e70a4e776482
gpoppe <gpoppe@csudh.edu>
2026-01-15 01:37:29 +0000

    Menu with 5 options

---
+++ gpoppe9.py
@@ -0,0 +1,50 @@
+#garrett poppe
+#github test comment
+
+name = "garrett"
+print("    Menu       ")
+print("_______________")
+
+print("option 1")
+print("option 2")
+print("option 3")
+print("option 4")
+print("option 5")
+
+print("________________")
+
+print("hello ",name," Enter an option")
+x = int(input())
+
+if (x == 1):
+    print("you chose option 1")
+elif (x == 2):
+    print("you chose option 2")
+elif (x == 3):
+    print("you chose option 3")
+elif (x ==4):
+    print("welcome to the guessing game")
+    guess = 101
+    while (guess != 65):
+        guess = int(input(" Enter your guess: "))
+        #range control for guess
+        while (guess < 0) or (guess > 100):
+            guess = int(input(" Enter your guess between 1 and 100: "))
+            if (guess < 0) or (guess > 100):
+                print("your guess is out of range")
+        
+        if (guess > 65):
+            print("Your guess is too high")
+        elif (guess < 65):
+            print("Your guess is too low")
+        else:
+            print("congratulations, you guess the number 65")
+
+elif (x == 5):
+    print("You chose option 5")
+
+
+
+
+
+
